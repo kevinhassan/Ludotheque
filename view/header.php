1b6ef032983cb4248ac9a945e291394183ab75ea
zambra.n <zambrano.nicolas@outlook.com>
2015-10-21 16:32:48 +0200

    Merge origin/master

--- view/header.php
+++ view/header.php
@@ -3,6 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap/style.css">
+        <link rel="stylesheet" href="css/bootstrap/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/bootstrap/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
         <title>Ludothèque</title>
     </head>
     <body>
